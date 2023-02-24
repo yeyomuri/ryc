b8ffafde32d5dbdc9c54cd48dc4b65fcfe3818a2
yeyomuri <yeyomuri@gmail.com>
2023-02-23 23:18:08 -0500

    modelo

---
+++ lib/model/category.dart
@@ -0,0 +1,12 @@
+class Category {
+  final String name;
+  final String image;
+  final int cost;
+  const Category({required this.name, required this.image, required this.cost});
+}
+
+//List of products
+
+final categoryList = [
+  const Category(name: 'Cy-gor', image: '', cost: 0),
+];
---
+++ lib/model/product.dart
@@ -0,0 +1,15 @@
+class Product {
+  final String name;
+  final String image;
+
+  const Product({required this.name, required this.image});
+}
+
+//List of products
+
+final productList = [
+  const Product(name: 'Cy-gor', image: ''),
+  const Product(name: 'Gecko', image: ''),
+  const Product(name: 'Litle Mice', image: ''),
+  const Product(name: 'Alien', image: ''),
+];
